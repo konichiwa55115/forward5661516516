e711bc887445e22598afcff5604361f1db21cae0
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-02-19 13:53:51 +0200

    Update config.py

--- forwarder/config.py
+++ forwarder/config.py
@@ -6,7 +6,7 @@ class Development(Config):
     OWNER_ID = 1227193881  # Your user id
 
     # Make sure to include the '-' sign in group and channel ids.
-    FROM_CHATS = [1227193881]  # List of chat id's to forward messages from.
+    FROM_CHATS = [1227193881,6234365091]  # List of chat id's to forward messages from.
     TO_CHATS = [-1001739160183]  # List of chat id's to forward messages to.
 
     REMOVE_TAG = True
